d04314d62dcc898ed9cd9c91f8a5fda93eb9c660
DavidsonCo <opeoluwa313@gmail.com>
2022-09-16 18:17:36 +0100

    Fixed Font Issue

--- todo/settings.py
+++ todo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import dj_database_url
 import django_heroku
@@ -137,3 +137,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 django_heroku.settings(locals())
 
 SITE_ID = 1
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
